f8af397b0bead1eb265a2ae1ebd882c9b855312f
David Villanueva <gdavidv1997@gmail.com>
2022-12-05 20:23:17 -0300

    add item log

--- src/store/actions/items.actions.ts
+++ src/store/actions/items.actions.ts
@@ -5,6 +5,7 @@ import { types } from "../../configs/types";
 import { IItem} from "../../models/item.model";
 import { deleteObject, getDownloadURL, ref, uploadString } from "firebase/storage";
 import { setDeleteLoading, unsetDeleteLoading } from "./ui.actions";
+import { getAnalytics, logEvent } from "firebase/analytics";
 
 export const startLoadingItems = ( uid: string ) => {
     return async ( dispatch: any) => {
@@ -56,6 +57,11 @@ export const startAddingItem = ( item: IItem, uid: string ) => {
         dispatch( setLoadingItem() )
         addDoc(collection(databaseRef,`${uid}/giftapp/items` ), item)
             .then( snap => {
+                const analytics = getAnalytics();
+                logEvent(analytics, 'add_item', {
+                    user: uid,
+                    item_id: snap.id
+                });
                 dispatch( addItem({
                     id: snap.id,
                     itemName: item.itemName,
